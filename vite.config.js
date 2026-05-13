1283eef2cc1410a264c0cb9f48e0ea6a59423d60
shiliu <user@shiliu.app>
2026-05-13 23:18:06 +0800

    fix: update base URL for repo name

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import { VitePWA } from 'vite-plugin-pwa'
 
 export default defineConfig({
-  base: '/shiliu/',
+  base: '/tomato-clock/',
   plugins: [
     VitePWA({
       registerType: 'autoUpdate',
